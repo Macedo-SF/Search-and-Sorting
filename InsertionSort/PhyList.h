ab77ecdbc54b4128c5346637abdad071a734007c
Saulo Macedo <72470930+Macedo-SF@users.noreply.github.com>
2022-01-04 20:55:54 -0300

    Name change

--- InsertionSort/PhyList.h
+++ InsertionSort/PhyList.h
@@ -7,15 +7,15 @@
 #include <ctime>
 #include <vector>
 
-class DynList
+class PhyList
 {
 private:
 	std::vector<int> List;
 public:
-	DynList(); //default constructor
-	DynList(const std::string &filename); //read from file
-	DynList(const unsigned int &size, const int &range, const int &start); //generate random list
-	~DynList(); //default destructor
+	PhyList(); //default constructor
+	PhyList(const std::string &filename); //read from file
+	PhyList(const unsigned int &size, const int &range, const int &start); //generate random list
+	~PhyList(); //default destructor
 	void insert(const int &key, const unsigned int &pos); //inserts key into pos
 	void swap(const int &p1, const int &p2); //swap p1 and p2
 	int find(const int &key) const; //returns the position of a given key, -1 if not found
